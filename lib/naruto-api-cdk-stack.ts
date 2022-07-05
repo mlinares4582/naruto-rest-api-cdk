e5c45d8e34ce4fa7fb6778cab2ea179295c72ad2
MichaelLinares <mglinares.2419@gmail.com>
2022-07-05 11:12:07 -0400

    first commit

--- lib/naruto-api-cdk-stack.ts
+++ lib/naruto-api-cdk-stack.ts
@@ -1,16 +1,48 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import { AttributeType, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
+import { join } from 'path';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 
 export class NarutoApiCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const narutoTable = new Table(this,'table-naruto',{
+      partitionKey: { name: "id", type: AttributeType.STRING },
+      tableName: "Table-Naruto"
+      
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'NarutoApiCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const getShinobiLambdaFunction = new NodejsFunction(this, 'getAllLambdaNarutoFunction', {
+      entry: join(__dirname, '../functions/get/index.js'),
+      functionName: 'get-all-naruto-function',
+      bundling:{
+        externalModules:[
+          'aws-sdk',
+        ]
+      },
+    environment: {
+      PRIMARY_KEY: "id",
+      TABLE_NAME: narutoTable.tableName,
+    }
+  });
+    //permission for lambda to access the table
+    narutoTable.grantReadWriteData(getShinobiLambdaFunction);
+
+
+
+
+    const api = new LambdaRestApi(this, 'naruto-api', {
+      restApiName: 'Naruto_Api',
+      handler: getShinobiLambdaFunction,
+      proxy: false,
+    });
+
+    const naruto = api.root.addResource('naruto');
+    naruto.addMethod('GET');
+
+    
   }
 }
